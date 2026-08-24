f285b3968ff4be16504607f689d8f852b9668a10
Jeffersonf <2104133@aluno.univesp.br>
2026-08-24 18:43:10 -0300

    refactor: importar base nativa do Finext para PainelURE

---
+++ android-kotlin/app/src/main/java/com/painelure/app/ui/theme/PainelAliases.kt
@@ -0,0 +1,12 @@
+package com.painelure.app.ui.theme
+
+// Aliases used by the PainelURE domain layer while retaining the Finext theme source.
+val Lime = AccentLime
+val Mint = SuccessGreen
+val Purple = AccentPurple
+val PanelSurface = SurfaceDark
+val Muted = TextSecondaryDark
+val MutedLight = TextSecondaryLight
+val TextDark = TextPrimaryDark
+val TextLight = TextPrimaryLight
+val PainelTypography = AppTypography
